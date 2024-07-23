1fb9d0d6dfe52415c939df04ef5d110cfb46c088
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-07-23 11:31:05 -0300

    refactor: Add rect field istead of x, y, w and h ones

--- src/main.c
+++ src/main.c
@@ -7,10 +7,8 @@
 #define BALL_SPEED 5.0f
 
 typedef struct Object {
-    Vector2 pos;
+    Rectangle rect;
     Vector2 dir;
-    float w;
-    float h;
     Color color;
 } Object;
 
@@ -18,51 +16,60 @@ int main(void) {
 
     // player
     Object pedal = {
-        .pos =  { .x = (SCREEN_WIDTH - PEDAL_WIDTH) / 2, .y = SCREEN_HEIGHT - 50.0},
+        .rect = (Rectangle) { 
+            .x = (SCREEN_WIDTH - PEDAL_WIDTH) / 2,
+            .y = SCREEN_HEIGHT - 50.0,
+            .width = PEDAL_WIDTH,
+            .height = 20.0
+        },
         .dir = { .x = 6.5, .y = 0.0 },
         .color = RED,
-        .w = PEDAL_WIDTH,
-        .h = 20.0,
     };
 
     Object ball = {
-        .pos = { 0.0, 0.0 },
+        .rect = (Rectangle) { 
+            .x = 0.0,
+            .y = 0.0,
+            .width = 20,
+            .height = 20
+        },
         .dir = { BALL_SPEED, BALL_SPEED },
         .color = GREEN,
-        .w = 20,
-        .h = 20,
     };
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "breakout");
     SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
-
-
         /* ==== PLAYER UPDATE ==== */
         if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
-            pedal.pos.x += pedal.dir.x;
+            pedal.rect.x += pedal.dir.x;
         if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
-            pedal.pos.x -= pedal.dir.x;
+            pedal.rect.x -= pedal.dir.x;
 
         // dont let the pedal go out of bounds
-        if (pedal.pos.x >= SCREEN_WIDTH - pedal.w) pedal.pos.x = SCREEN_WIDTH - pedal.w;
-        if (pedal.pos.x <= 0) pedal.pos.x = 0;
+        if (pedal.rect.x >= SCREEN_WIDTH - pedal.rect.width) 
+            pedal.rect.x = SCREEN_WIDTH - pedal.rect.width;
+        if (pedal.rect.x <= 0) 
+            pedal.rect.x = 0;
 
         /* ==== BALL UPDATE ==== */
         // normalize ball dir vector for fixing it diagonal movement, 
         // the, scale it 
         Vector2 dir = Vector2Scale(Vector2Normalize(ball.dir), 5.0);
-        ball.pos  = Vector2Add(ball.pos, dir);
+        Vector2 new_ball_pos  = Vector2Add((Vector2){ ball.rect.x, ball.rect.y}, dir);
+        ball.rect.x = new_ball_pos.x;
+        ball.rect.y = new_ball_pos.y;
 
-        if (ball.pos.x >= SCREEN_WIDTH - ball.w || ball.pos.x <= 0)
+        if (ball.rect.x >= SCREEN_WIDTH - ball.rect.width || ball.rect.x <= 0)
             ball.dir.x *= -1.0;
 
-        if (ball.pos.y >= SCREEN_HEIGHT - ball.h || ball.pos.y <= 0)
+        if (ball.rect.y >= SCREEN_HEIGHT - ball.rect.height || ball.rect.y <= 0)
             ball.dir.y *= -1.0;
 
         BeginDrawing();
-            DrawRectangle(pedal.pos.x, pedal.pos.y, pedal.w, pedal.h, pedal.color);
-            DrawRectangle(ball.pos.x, ball.pos.y, ball.w, ball.h, ball.color);
+            DrawRectangleRec(pedal.rect, pedal.color);
+            DrawRectangleRec(ball.rect, ball.color);
             ClearBackground(RAYWHITE);
         EndDrawing();
     }
