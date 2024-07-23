793e09631428aaae5ad0ce05b16292e443b5745c
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-07-23 10:54:21 -0300

    feat: Add a player's pedal

--- src/main.c
+++ src/main.c
@@ -1,19 +1,38 @@
 #include "raylib.h"
+#include "raymath.h"
 
 #define SCREEN_WIDTH 720
 #define SCREEN_HEIGHT 420
 
+typedef struct Object {
+    Vector2 pos;
+    float speed;
+    float w;
+    float h;
+    Color color;
+} Object;
+
 int main(void) {
+
+    // player
+    Object pedal = {
+        .pos =  { .x = 50.0, .y = SCREEN_HEIGHT - 50.0},
+        .speed = 5.0,
+        .color = RED,
+        .w = 100.0,
+        .h = 20.0,
+    };
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "breakout");
     SetTargetFPS(60);
-
     while (!WindowShouldClose()) {
         BeginDrawing();
+            DrawRectangle(pedal.pos.x, pedal.pos.y, pedal.w, pedal.h, pedal.color);
             ClearBackground(RAYWHITE);
-            DrawText("Hello, World!", SCREEN_WIDTH / 2.5, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
+
+    CloseWindow();
 
     return 0;
 }
